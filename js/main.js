6e5104bd58d36f49fb39ec6c4ed852b56d437a3c
Anastasia <167119194+wanesssssty@users.noreply.github.com>
2024-08-24 11:33:55 +0300

    version-2
    changed the file location

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.header').height($(window).height());
+})
+
+// // Initialization for ES Users
+// import { Collapse, Ripple, initMDB } from "mdb-ui-kit";
+
+// initMDB({ Collapse, Ripple });
