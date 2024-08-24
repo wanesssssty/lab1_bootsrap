375d5abe2c85b93f4ef3e38c68e0da0e33355483
Anastasia <167119194+wanesssssty@users.noreply.github.com>
2024-08-24 11:35:12 +0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.header').height($(window).height());
-})
-
-// // Initialization for ES Users
-// import { Collapse, Ripple, initMDB } from "mdb-ui-kit";
-
-// initMDB({ Collapse, Ripple });
